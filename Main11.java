2fe28e0b09fb29aa86b7b6298ded1fa1ab335fda
Caio de Castro Coelho <caiocoelho2003@gmail.com>
2026-04-28 20:26:55 -0300

    Add files via upload

---
+++ Main11.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main11 {
+  public static void main (String[] args) {
+  
+    Scanner scanner = new Scanner(System.in);
+
+     double saldo = 200.00;
+     double saque;
+     double deposito;
+
+    System.out.println("Aperte a tecla l para ver seu saldo \n Aperte a tecla g para fazer um saque \n Aperte a tecla u para fazer um deposito");
+   
+    String tecla = scanner.nextLine();
+
+    if (tecla.equals( "l")) {
+      System.out.println("seu saldo é " + saldo);
+    } else if (tecla.equals("g")) {
+        System.out.print("qual o valor do saque?");
+        saque = scanner.nextDouble();
+        scanner.nextLine();
+       if (saque <= saldo) {
+        System.out.println("3\n2 \n 1\nsaque completo");
+        saldo -= saque;
+        System.out.println("seu saldo agora é " + saldo);
+       } else {
+        System.out.println("você não pode sacar mais do que tem");
+       }
+    } else if (tecla.equals("u")) {
+        System.out.print("qual o valor do deposito?");
+        deposito = scanner.nextDouble();
+      scanner.nextLine();
+        System.out.println("3\n2\n1\ndeposito concluido");
+        saldo += deposito;
+        System.out.println("seu saldo agora é " + saldo);
+    } else {
+        System.out.println("comando não reconhecido");
+    }
+    scanner.close();
+  }
+}
